c7740f16af46300fd06d23f886b85c158d7fe568
Zaid Nawaz <zaidalviza786@gmail.com>
2026-02-13 03:18:54 +0500

    Performance improveemnets fixes

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -61,6 +61,16 @@ const CardContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
-CardContent.displayName = "CardContent"
+const CardFooter = React.forwardRef<
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+    <div
+        ref={ref}
+        className={cn("flex items-center p-6 pt-0", className)}
+        {...props}
+    />
+))
+CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
